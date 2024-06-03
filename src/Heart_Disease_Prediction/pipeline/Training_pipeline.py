00071fb4cfd19916a79cf885d1580bfe103a51f0
Sakshi Choube <sakshichoube4232gmail.com>
2024-06-03 23:59:39 +0530

    files updated

--- src/Heart_Disease_Prediction/pipeline/Training_pipeline.py
+++ src/Heart_Disease_Prediction/pipeline/Training_pipeline.py
@@ -1,7 +1,6 @@
 from src.Heart_Disease_Prediction.components.Data_ingestion import DataIngestion
 from src.Heart_Disease_Prediction.components.Data_transformation import DataTransformation
 from src.Heart_Disease_Prediction.components.Model_trainer import ModelTrainer
-from src.Heart_Disease_Prediction.components.Model_evaluation import ModelEvaluation
 
 
 #Data ingestion Pipeline
@@ -16,6 +15,3 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
 
-# Model Evaluation Pipeline
-model_eval_obj = ModelEvaluation()
-model_eval_obj.initate_model_evaluation(train_arr,test_arr)
